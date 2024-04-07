7cf06c590ef809a340a1fb9d37a72b96eaa64aa8
Monish Thirukkumaran <monish.thir@gmail.com>
2024-04-07 22:37:45 +0100

    feat: skip vec allocation on walkdir result

--- src/walker.rs
+++ src/walker.rs
@@ -8,17 +8,16 @@ pub(crate) fn walk_path(
     min_depth: Option<usize>,
     max_depth: Option<usize>,
     skip_files: bool,
-) -> color_eyre::Result<Vec<String>> {
+) -> color_eyre::Result<impl IntoIterator<Item = String>> {
     // Canonicalize the path to get absolute path.
     Ok(WalkDir::new(std::fs::canonicalize(path)?)
         .max_depth(max_depth.unwrap_or(1))
         .min_depth(min_depth.unwrap_or(1))
         .into_iter()
-        .filter_entry(|e| {
+        .filter_entry(move |e| {
             e.metadata()
                 .map_or(false, |meta| if skip_files { meta.is_dir() } else { true })
         })
         .flatten()
-        .map(|e| e.path().display().to_string())
-        .collect::<Vec<String>>())
+        .map(|e| e.path().display().to_string()))
 }
